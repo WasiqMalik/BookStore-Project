9889b1e2e6aede4f3c3ff7001b9cf965e30a94a0
Wasiq Malik <wasiq.malik13@gmail.com>
2019-05-03 00:25:32 +0500

    User Controller Merge

--- DB_Project/Controllers/UserController.cs
+++ DB_Project/Controllers/UserController.cs
@@ -30,7 +30,26 @@ namespace DB_Project.Controllers
             return View("~/Views/User/DashBoard.cshtml", BookCRUD.TitleSearch(svalue));
         }
 
-        
+        public ActionResult BookDetails(int id)
+        {
+            return View(BookCRUD.GetBookReviews(id, (int)Session["UserID"]));
+        }
+
+        [HttpPost]
+        public ActionResult ReviewBook(FormCollection collection)
+        {
+            Review newReview = new Review();
+            newReview.UserID = (int)Session["UserID"];
+            newReview.UserName = (string)Session["UserName"];
+            newReview.BookID = Int32.Parse(collection["BookID"]);
+            newReview.Description = collection["ReviewText"];
+            newReview.Rating = Int32.Parse(collection["Rating"]);
+
+            if (ReviewCRUD.CreateReview(newReview))
+                return Content("<script>alert('Review has been added Successfully.');window.location.reload();</script>");
+            else
+                return Content("<script>alert('Review Failed.');window.location.reload();</script>");
+        }
 
     }
 }
